aeada4488ac97ba0f5a105898762691d1c3d3598
Roque <164688618+PdRoque@users.noreply.github.com>
2025-08-29 20:12:20 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -25,4 +25,12 @@ elif sinal == "/":
 else:
     print("Operação inválida. Por favor, escolha +, -, * ou /.")
 
+#if = se
+#O if é a base da estrutura. Ele avalia uma condição e, se ela for verdadeira, executa um bloco de código.
+
+#else = se não
+#O else é opcional e funciona como um "plano B". Ele é executado apenas quando a condição do if é falsa.
+
+#elif = se não, se
+#O elif é a combinação de "else" e "if". Ele permite que você verifique múltiplas condições
 
