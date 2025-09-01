7b3119ff108b3f6452f41209da8ff0d44cdbbe2d
Roque <164688618+PdRoque@users.noreply.github.com>
2025-09-01 19:30:49 -0300

    Create maior_número.py

---
+++ maior_número.py
@@ -0,0 +1,15 @@
+print("**********")
+print("escolhendo o maior número")
+print("**********")
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+print("**********")
+
+if num1 >= num2 and num1 >= num3:
+    print(f"o maior número é: {num1}")
+elif num2 >= num1 and num2 >= num3:
+    print(f"O maior número é: {num2}")
+else:
+    print(f"O maior número é: {num3}")
