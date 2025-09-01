b6be1fe94dbce3d6f3a69e4933a368c67acb8f7c
Roque <164688618+PdRoque@users.noreply.github.com>
2025-09-01 19:29:35 -0300

    Create celsius_para_fahrenheit.py

---
+++ celsius_para_fahrenheit.py
@@ -0,0 +1,9 @@
+print("**********")
+print("CONVERTENDO CELSIUS PARA FAHRENHEIT")
+print("**********")
+
+celsius = int(input("Digite a temperatura em C: "))
+
+calculo = (celsius * 9 / 5) + 32
+
+print(f"{celsius} em graus celsius convertido em fahrenhait é {calculo}")
